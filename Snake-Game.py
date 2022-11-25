72ba95e71ea5d15e993960dae47bca5b3c30898e
FRIDAHCHEBOI01 <fridah.cheboi@ashesi.edu.gh>
2022-11-25 14:43:04 +0000

    new change

--- Snake-Game.py
+++ Snake-Game.py
@@ -1,6 +1,7 @@
 # Start coding snake game in Python
 # using the turtle module library
 from turtle import *
+import time #time module used to control speed of the snake head bb:)
 #creating an object from the screen class
 my_game = Screen()
 my_game.title("Original Snake game :)")
@@ -27,14 +28,47 @@ text.goto(0,210)
 
 text.hideturtle()
 text.color('white')
-text.write('Best Game Everrr :)', font = ('roman',13,'bold'),align ='center')
+text.write('Best Game Everrr. No lie:)', font = ('roman',13,'bold'),align ='center')
 
+'''
+After setting the setup for my game I will now write code to move the snake on the screen.
+'''
+def movesnake():
+    if snakehead.direction == "up":
+        y = snakehead.ycor()
+        y += 20
+        snakehead.sety(y)
+    if snakehead.direction == "down":
+        y = snakehead.ycor()
+        y-=20
+        snakehead.sety(y)
+    if snakehead.direction == "right":
+        x = snakehead.xcor()
+        x+=20
+        snakehead.setx(x)
+    if snakehead.direction == "left":
+        x = snakehead.xcor()
+        x-=20
+        snakehead.setx(x)
 
+def upward():
+    snakehead.direction = "up"
+def downward():
+    snakehead.direction = "down"
+def right():
+    snakehead.direction = "right"
+def left():
+    snakehead.direction = "left"
+my_game.listen()
 
+my_game.onkeypress(upward,"up")
 
 
 while True:
     #update method from the screen class holds the screen for long:)
     my_game.update()
+    movesnake()
+    time.sleep(1)
+
 
 
